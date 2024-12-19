0bafe18b98cb25264196f704199e9f18b861755d
into the deep <santi26011830@gmail.com>
2024-12-18 23:13:52 -0600

    commit para rodrigo arellano 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleopEscalador.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleopEscalador.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Commands.EscaladorDEfaulBajarTeleop;
 import org.firstinspires.ftc.teamcode.Commands.EscaladorDefault;
+import org.firstinspires.ftc.teamcode.Commands.SubirEscalador;
 import org.firstinspires.ftc.teamcode.Subsystem.Escalador;
 
 @TeleOp
@@ -16,5 +17,6 @@ public class TeleopEscalador extends CommandOpMode {
         Escalador escalador = new Escalador(hardwareMap, telemetry);
         GamepadEx gamepadSecond = new GamepadEx(gamepad2);
         escalador.setDefaultCommand(new EscaladorDEfaulBajarTeleop(escalador, gamepadSecond));
+        escalador.setDefaultCommand(new SubirEscalador(escalador, gamepadSecond));
     }
 }
